47ce7ecfbd4d94a24a1e8967fdadda4c036b5403
Raja <rdas6313@gmail.com>
2025-12-14 20:35:21 +0530

    changed shared buffer size to 10

--- test/Test_Headers.h
+++ test/Test_Headers.h
@@ -1,11 +1,16 @@
 #ifndef HEAD_TEST
 #define HEAD_TEST
 
+#ifdef LINUX
+#define MACHINE_IP "192.168.0.122"
+#else
 #define MACHINE_IP "192.168.0.179"
+#endif
+
 #define MULTICAST_PORT 50002
 #define MULTICAST_IP "235.0.0.1"
 #define MACHINE_PORT MULTICAST_PORT
-#define SHARED_BUFFER_SIZE (10 * MAX_FRAME_SIZE)
+#define SHARED_BUFFER_SIZE (10 * sizeof(VideoFrame))
 
 #define SHARED_KEY "buf"
 
